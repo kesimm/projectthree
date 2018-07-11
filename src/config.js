a448288f4c1a8bda5ea712afc8b51027d7e50c28
kesimm <kenzie.simmons27@gmail.com>
2018-07-11 16:12:55 -0600

    spelling error

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiUrl = 'mongodb://heroku_pbbdj41v:ibobc5bdgbnmv8s7capj8aj94l@ds151180.mlab.com:51180/heroku_pbbdj41v'
+const apiUrl = 'https://shrouded-garden-69575.herokuapp.com/api'
 const siteUrl = 'https://shrouded-garden-69575.herokuapp.com/'
 
 export { apiUrl, siteUrl };
