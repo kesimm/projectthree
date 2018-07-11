c2abbcc166e2ddcd0c2aa46791fcb9516c0fb37b
kesimm <kenzie.simmons27@gmail.com>
2018-07-11 15:50:02 -0600

    still not working

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://api.mlab.com/api/1/databases?apiKey=LM9HG95UnI4YiBhbl9Iw9mD14r_IJvud'
+const apiUrl = 'https://api.mlab.com/api/1/databases/projectthree?apiKey=LM9HG95UnI4YiBhbl9Iw9mD14r_IJvud'
 const siteUrl = 'https://shrouded-garden-69575.herokuapp.com/'
 
 export { apiUrl, siteUrl };
