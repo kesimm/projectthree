e3a7e72c471d1cde40a0d2b1cea9823d1ef1ba86
kesimm <kenzie.simmons27@gmail.com>
2018-07-11 17:04:41 -0600

    try this again

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ if ( dev ) {
 	siteUrl = 'http://localhost:3000';
 	dbHost = 'mongodb://kesimm:Quince27@ds131971.mlab.com:31971/projectthree';
 } else {
-	siteUrl = 'https://shrouded-garden-69575.herokuapp.com/';
+	siteUrl = 'https://dot-connect.sjsakib.me';
 	dbHost = 'process.env.LIVE_DB';
 }
 
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const apiUrl = 'https://dot-connect-server.now.sh'
-const siteUrl = 'https://shrouded-garden-69575.herokuapp.com/'
+const siteUrl = 'https://dot-connect.sjsakib.me'
 
 export { apiUrl, siteUrl };
