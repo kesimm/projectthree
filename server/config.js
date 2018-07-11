cb26800c6b9b7d442466159a21f0d1e4049e5e1c
kesimm <kenzie.simmons27@gmail.com>
2018-07-11 15:06:32 -0600

    added db to config

--- server/config.js
+++ server/config.js
@@ -4,9 +4,9 @@ const dev = true;
 
 if ( dev ) {
 	siteUrl = 'http://localhost:3000';
-	dbHost = 'mongodb://kesimm:Quince27@ds131971.mlab.com:31971/projectthree';
+	dbHost = 'mongodb://kesimm:Quince27@ds233581.mlab.com:33581/heroku_bs8zhnfp';
 } else {
-	siteUrl = '';
+	siteUrl = 'https://shrouded-garden-69575.herokuapp.com/';
 	dbHost = process.env.LIVE_DB;
 }
 
