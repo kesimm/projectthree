11fec03da8d957f84bd056585e1f1807a475f82c
kesimm <kenzie.simmons27@gmail.com>
2018-07-11 16:41:27 -0600

    nope

--- server/config.js
+++ server/config.js
@@ -1,13 +1,13 @@
 let siteUrl, dbHost;
 
-const dev = false;
+const dev = true;
 
 if ( dev ) {
 	siteUrl = 'http://localhost:3000';
 	dbHost = 'mongodb://kesimm:Quince27@ds131971.mlab.com:31971/projectthree';
 } else {
 	siteUrl = 'https://shrouded-garden-69575.herokuapp.com/';
-	dbHost = 'mongodb://heroku_pbbdj41v:Quince27@ds151180.mlab.com:51180/heroku_pbbdj41v';
+	dbHost = 'mongodb://kesimm:Quince27@ds131971.mlab.com:31971/projectthree';
 }
 
 module.exports = { siteUrl, dbHost };
