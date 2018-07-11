3ec2668cfb021fbabe0a56e3a07c1462ad4a119c
kesimm <kenzie.simmons27@gmail.com>
2018-07-11 17:00:16 -0600

    Try this

--- server/config.js
+++ server/config.js
@@ -7,7 +7,7 @@ if ( dev ) {
 	dbHost = 'mongodb://kesimm:Quince27@ds131971.mlab.com:31971/projectthree';
 } else {
 	siteUrl = 'https://shrouded-garden-69575.herokuapp.com/';
-	dbHost = 'mongodb://kesimm:Quince27@ds131971.mlab.com:31971/projectthree';
+	dbHost = 'process.env.LIVE_DB';
 }
 
 module.exports = { siteUrl, dbHost };
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://shrouded-garden-69575.herokuapp.com/api'
+const apiUrl = 'https://dot-connect-server.now.sh'
 const siteUrl = 'https://shrouded-garden-69575.herokuapp.com/'
 
 export { apiUrl, siteUrl };
