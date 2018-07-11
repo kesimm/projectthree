22a19babb52647d971dd70d1de890d2fff2104c8
kesimm <kenzie.simmons27@gmail.com>
2018-07-11 16:06:06 -0600

    maybe?

--- server/config.js
+++ server/config.js
@@ -7,7 +7,7 @@ if ( dev ) {
 	dbHost = 'mongodb://kesimm:Quince27@ds131971.mlab.com:31971/projectthree';
 } else {
 	siteUrl = 'https://shrouded-garden-69575.herokuapp.com/';
-	dbHost = 'mongodb://heroku_pbbdj41v:Quince27>@ds151180.mlab.com:51180/heroku_pbbdj41v';
+	dbHost = 'mongodb://heroku_pbbdj41v:ibobc5bdgbnmv8s7capj8aj94l@ds151180.mlab.com:51180/heroku_pbbdj41v';
 }
 
 module.exports = { siteUrl, dbHost };
